9a59cc3ac8478fcca42149aa798294dc34ad062e
tchowdhu75209 <65376152+tchowdhu75209@users.noreply.github.com>
2020-06-02 18:32:52 -0500

    Create taicode.R

---
+++ demo code/taicode.R
@@ -0,0 +1,6 @@
+#Tai's Code's for Project one
+#After importing data, need to clean up both projection and model files by removing "NA"s
+#Author Tai Chow
+
+newprojection <- na.omit(projectio)
+newmodeling <-na.omit(modelingData)
